7ac28466a98dffdfeb3c158d04e65684e0157a06
khaledmv <khaledhossionmv@gmail.com>
2023-05-02 15:59:22 +0600

    initialized react context shoppingCart

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import { AllRoutes } from "./routes/AllRoutes";
+import { Header } from "./components";
+
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <div className="App">
+        <Header />
+        <AllRoutes />
+      </div>
+    </>
+  )
+}
+
+export default App
